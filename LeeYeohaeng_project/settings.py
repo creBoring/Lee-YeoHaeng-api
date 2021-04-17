c3d92659fa51c064ee71eaa0c7e00c053985ae1f
Dohun Lee <cre.boring@gmail.com>
2021-04-17 11:51:34 +0900

    import mysqlclientlib for external db

--- LeeYeohaeng_project/settings.py
+++ LeeYeohaeng_project/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -80,8 +79,10 @@ WSGI_APPLICATION = 'LeeYeohaeng_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'LeeYeohaeng_project/db_auth.cnf')
+        }
     }
 }
 
