dbde8999052114d1c0ef6719807104071b3b66a4
creBoring <cre.boring@gmail.com>
2021-11-13 01:13:37 +0900

    MySQL DB Config 구성 - MySQL 서버에 접근을 위한 config 파일을 구성했습니다 - DB Credential 정보의 경우 보안을 위해 AWS System Manager Parameter Store에 저장해두었으며, AWS Library의 API를 통해 데이터를 불러들입니다.

--- config/db.config.js
+++ config/db.config.js
@@ -1,1 +1,43 @@
-// some config file
+const mysql = require('mysql');
+const AWS = require("aws-sdk");
+AWS.config.update({region: 'ap-northeast-2'});
+
+/*
+ * SSM Parameter Store를 통해 암호화된 DB Credential 가져오기
+ */
+const ssm = AWS.SSM();
+const params = {
+  host: {
+    Name: '/lyh/db/writer-host',
+    WithDecryption: true
+  },
+  user: {
+    Name: '/lyh/db/user',
+    WithDecryption: true
+  },
+  password: {
+    Name: '/lyh/db/password',
+    WithDecryption: true
+  },
+  database: {
+    Name: '/lyh/db/database',
+    WithDecryption: true
+  }
+}
+
+const host = await ssm.getParameter(params.host);
+const user = await ssm.getParameter(params.user);
+const password = await ssm.getParameter(params.password);
+const database = await ssm.getParameter(params.database);
+
+const db = mysql.createConnection({
+  host: host,
+  user: user,
+  password: password,
+  port: 3306,
+  database: database
+});
+
+db.connect();
+
+module.exports = db;
