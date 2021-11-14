fe6e78d2f6a8f3d3e4bc75e393ec2982323f06c8
creBoring <cre.boring@gmail.com>
2021-11-14 23:23:46 +0900

    Session 추가 - 로그인 구현에 앞서, Session 핸들링을 위해 express-session 라이브러리를 추가하고 사용하였습니다

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const PORT = 3000
 
 const authRouter = require('./routes/auth');
 
+// express 옵션들
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// session
+app.use(session({
+  secret: 'l22Salt_!@#so$CutE',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/auth', authRouter);
 app.listen(PORT, () => console.log('Lee YeoHaeng API Server listening on port 3000'))
